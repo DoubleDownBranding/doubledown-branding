c1720bbf0905abf10bf7566f2dc2bd10471a90d5
DoubleDownBranding <chriscalawaybranding@gmail.com>
2025-08-17 21:22:19 +0330

    Switch to static build and enable sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import netlify from '@astrojs/netlify';
 
 export default defineConfig({
-  output: 'server',
-  adapter: netlify(),
-  site: 'https://doubledownbranding.netlify.app'
+  site: 'https://doubledownbranding.com',
+  integrations: [sitemap()],
 });
